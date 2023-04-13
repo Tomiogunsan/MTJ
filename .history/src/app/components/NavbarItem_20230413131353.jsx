983d1e9c7109c2d37d8b7d742791433c8c9dba80
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-13 13:14:36 +0100

    fix

---
+++ .history/src/app/components/NavbarItem_20230413131353.jsx
@@ -0,0 +1,16 @@
+'use client'
+
+import Link from 'next/link'
+import React from 'react'
+import {useSearchParams} from 'next/navigation'
+
+export default function NavbarItem({title, param}) {
+    const searchParams = useSearchParams()
+    const genre = searchParams.get('genre')
+  return (
+    <div>
+        
+        
+    </div>
+  )
+}
