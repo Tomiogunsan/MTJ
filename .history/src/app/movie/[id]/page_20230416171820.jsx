1564fc0cc195e46553bc5e3a20f77be01d5ec568
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-16 17:18:34 +0100

    refactor

--- .history/src/app/movie/[id]/page_20230416171820.jsx
+++ .history/src/app/movie/[id]/page_20230416171820.jsx
@@ -20,23 +20,7 @@ export default async function Moviepage({params}) {
           blurDataURL="/spinner.svg"
           className=" rounded-lg"
         />
-        <div className="p-2">
-          <h2 className="text-lg mb-3 font-bold">
-            {movie.title || movie.name}
-          </h2>
-          <p className="text-lg mb-3">
-            <span className="font-semibold mr-1">Overview:</span>
-            {movie.overview}
-          </p>
-          <p className="mb-3 ">
-            <span className="font-semibold mr-1">Date Released:</span>
-            {movie.release_date || movie.first_air_date}
-          </p>
-          <p className="mb-3 ">
-            <span className="font-semibold mr-1"> Rating:</span>
-            {movie.vote_count}
-          </p>
-        </div>
+      
       </div>
     </div>
   );
