f3edb6c2a73e3aeea0477e49a73eb89de2660613
Tomi Ogunsan <tomiafolayan@gmail.com>
2023-04-16 17:18:12 +0100

    refactor

--- .history/src/app/movie/[id]/page_20230416171755.jsx
+++ .history/src/app/movie/[id]/page_20230416171755.jsx
@@ -8,18 +8,7 @@ export default async function Moviepage({params}) {
   return (
     <div className="w-full ">
       <div className="p-4 md:pt-8 flex  flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
-        <Image
-          src={`https://image.tmdb.org/t/p/original/${
-            movie.backdrop_path || movie.poster_path
-          }`}
-          width={500}
-          height={300}
-          style={{ maxWidth: "100%", height: "100%" }}
-          alt="Movie poster"
-          placeholder="blur"
-          blurDataURL="/spinner.svg"
-          className=" rounded-lg"
-        />
+        
         <div className="p-2">
           <h2 className="text-lg mb-3 font-bold">
             {movie.title || movie.name}
